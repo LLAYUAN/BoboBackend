227d1f9c15369cd2b16e4ccb4bd14dc0a6c16a86
DyingCoderLin <904573025@qq.com>
2024-06-28 14:37:32 +0800

    project structure

---
+++ liveVideo-service/src/main/java/org/example/livevideoservice/LiveVideoServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.livevideoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveVideoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveVideoServiceApplication.class, args);
+    }
+
+}
