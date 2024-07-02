0413abf435561acf95e8a664922c48d948d7b27a
h12321h <1656344825@qq.com>
2024-07-02 10:06:53 +0800

    add chat roomID

--- message-service/src/main/java/org/example/messageservice/controller/ChatController.java
+++ message-service/src/main/java/org/example/messageservice/controller/ChatController.java
@@ -10,17 +10,19 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-        System.out.println("Received message: " + chatMessage.getContent());
+    @MessageMapping("/chat.sendMessage/{roomID}")
+    @SendTo("/topic/public/{roomID}")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        String roomNumber = headerAccessor.getDestination().split("/")[3];
+        System.out.println("Received message in room " + roomNumber + ": " + chatMessage.getContent());
         return chatMessage;
     }
 
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chatMessage) {
-        System.out.println("Received addUser: " + chatMessage.getSender());
+    @MessageMapping("/chat.addUser/{roomID}")
+    @SendTo("/topic/public/{roomID}")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        String roomNumber = headerAccessor.getDestination().split("/")[3];
+        System.out.println("Received addUser in room " + roomNumber + ": " + chatMessage.getSender());
         chatMessage.setContent(chatMessage.getSender() + " joined");
         return chatMessage;
     }
