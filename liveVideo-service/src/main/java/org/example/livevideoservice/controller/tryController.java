7985719a28794db8bc550cc7e33efccea0481453
DyingCoderLin <904573025@qq.com>
2024-07-03 20:29:24 +0800

    每个服务块都加上了

---
+++ liveVideo-service/src/main/java/org/example/livevideoservice/controller/tryController.java
@@ -0,0 +1,22 @@
+package org.example.livevideoservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RefreshScope
+@RestController
+public class tryController {
+    @GetMapping(value = "/test")
+    public String test() {
+        return "test";
+    }
+}
