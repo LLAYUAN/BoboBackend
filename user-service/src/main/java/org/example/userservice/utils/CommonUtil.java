615ee7b2195e5ba82f76715e5a21f3d451f54b19
DyingCoderLin <904573025@qq.com>
2024-07-08 16:56:16 +0800

    修改bug

--- user-service/src/main/java/org/example/userservice/utils/CommonUtil.java
+++ user-service/src/main/java/org/example/userservice/utils/CommonUtil.java
@@ -4,8 +4,11 @@ import java.sql.Date;
 
 public class CommonUtil {
     public static Date stringToDate(String date) {
-        if(date == null)
+//        System.out.println("date: " + date);
+        if (date == null || date.trim().isEmpty()) {
+            System.out.println("Date string is null or empty");
             return null;
+        }
         return Date.valueOf(date);
     }
 }
