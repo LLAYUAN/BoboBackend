4883c544e59b01e7df7d3dfbf2810b89b26dc10a
DyingCoderLin <904573025@qq.com>
2024-08-07 16:51:59 +0800

    修改了isstatus删除后的一系列逻辑，优化了首页界面

--- liveVideo-service/src/main/java/org/example/livevideoservice/Feign/Feign.java
+++ liveVideo-service/src/main/java/org/example/livevideoservice/Feign/Feign.java
@@ -1,5 +1,6 @@
 package org.example.livevideoservice.Feign;
 
+import cn.hutool.json.JSONObject;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +12,11 @@ import java.util.Map;
 
 @FeignClient(name = "gateway")
 public interface Feign {
-    @PostMapping("/internal/user/setStatus")
-    void setStatus(@RequestBody Map<String, Object> requestBody);
+//    @PostMapping("/internal/user/setStatus")
+//    void setStatus(@RequestBody Map<String, Object> requestBody);
+
+    @PostMapping("/internal/recommend/roomInfo/createRoomHotIndex")
+    void createRoomHotIndex(@RequestBody JSONObject request);
 
     @DeleteMapping("/internal/message/delete/{roomID}")
     void deleteRoomMessages(@PathVariable Integer roomID);
