a146ef26aac24115c0194d1adfa67670f812af02
LLAYUAN <2235901996@qq.com>
2024-08-08 13:08:12 +0800

    delete some static

--- message-service/src/main/java/org/example/messageservice/config/CorsConfig.java
+++ message-service/src/main/java/org/example/messageservice/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000")  // 明确列出允许的域名或模式
+                .allowedOriginPatterns("https://123.60.73.77:3000")  // 明确列出允许的域名或模式
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
