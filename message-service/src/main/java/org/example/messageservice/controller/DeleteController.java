c95825ab4b0a5d745ecb98798d3e16ee20c37bbb
zjh <zhangjh1901@sjtu.edu.cn>
2024-08-09 12:59:56 +0800

    debug

--- message-service/src/main/java/org/example/messageservice/controller/DeleteController.java
+++ message-service/src/main/java/org/example/messageservice/controller/DeleteController.java
@@ -5,15 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DeleteController {
     @Autowired
     ChatService chatService;
 
     @DeleteMapping("/delete/{roomID}")
     public void deleteRoomMessages(@PathVariable Integer roomID) {
-        
+
         System.out.println("Received request to delete messages for roomID: " + roomID);
 
         chatService.deleteRoomMessages(roomID);
